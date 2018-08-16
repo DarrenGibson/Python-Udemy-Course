2ad19064d05c7ab20e707082dc389b89687fa684
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-16 15:14:36 -0700

    Add files via upload

---
+++ falseevaluations.py
@@ -0,0 +1,52 @@
+#=================================================================#
+#					   false evaluations
+#-----------------------------------------------------------------#
+
+'''
+Date		: Tue 14 Aug 2018 04:02:21 PM PDT 
+Author		: Darren Gibson
+Title		: Python Udemy Course 
+Resources	: https://www.udemy.com/python-the-complete-python-developer-course/learn/v4/t/lecture/3812186?start=0
+'''
+
+'''
+NOTES:
+
+
+'''
+
+print("""false:			{}
+none:					{}
+0:						{}
+0.0:					{}
+empty list			[]: {}
+empty tuple			(): {}
+empty string		'': {}
+empty string		"": {}
+empty mapping	  {{}}: {}
+""".format(False, bool(None), bool(0), bool(0.0), bool([]), bool(()), bool(''), bool(""), bool({})))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
