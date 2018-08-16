8ce5286a361a3db02ac72e24240ac674dbce0795
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-16 16:40:51 -0700

    Add files via upload

---
+++ NorwegianBlue.py
@@ -0,0 +1,45 @@
+#=================================================================#
+#					        Norwegian Blue  
+#-----------------------------------------------------------------#
+
+'''
+Date		: Tue 14 Aug 2018 04:02:21 PM PDT 
+Author		: Darren Gibson
+Title		: Norwegian Blue
+Resources	: https://www.udemy.com/python-the-complete-python-developer-course/learn/v4/t/lecture/3812186?start=0
+'''
+
+# Norwegian Blue
+
+'''
+NOTES:
+
+'''
+
+
+parrot = "Norwegian Blue"
+
+letter = input("enter a character:")
+
+if letter in parrot:
+	print("give me an {}, Bob".format(letter))
+else:
+	print("I dont need that letter")
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
