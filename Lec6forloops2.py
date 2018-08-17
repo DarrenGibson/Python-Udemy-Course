89e2ddbceda44eacfa75e3eb8cb90018d70ba7bd
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-17 16:45:27 -0700

    Add files via upload

--- Lec6forloops2.py
+++ Lec6forloops2.py
@@ -13,9 +13,42 @@ Resources	: https://www.udemy.com/python-the-complete-python-developer-course/le
 
 '''
 NOTES:
+range is a sequence type,     we put data in?!?!?
 
 '''
 
+# 
+
+number = '9,223,372,036,854,775,807'
+cleanedNumber = ''
+
+for char in number:
+	if char in '0123456789':
+		cleanedNumber = cleanedNumber + char
+		
+newNumber = int(cleanedNumber)
+print('the number is {}'.format(newNumber))
+
+# we can index trough lists like this
+for j in ['not pinin', 'no more', 'a stiff', 'breath of life']:
+# two good output options
+	print('this parrot is ' + j)
+	print('this parrot is {} '.format(j))
+
+# there is another argument/parameter?! available in our for loop
+# that lets us step trough our values?indexes?parameters?
+for i in range(0, 100, 5):
+	print('i is {} '.format(i))
+
+for i in range(1, 13):
+	for j in range(1, 13):
+# i have included some spacing after {} but it doesn't completely resovle the issue
+		print('{}   times {}   is {}'.format(i, j, i*j))
+# notice when we place this print function back with the first for command it
+# only apppears every 12th line
+	print('=========================')
+
+# Lecture 34, extract capitals challange
 
 
 
---
+++ forloopchallange.py
@@ -0,0 +1,104 @@
+#==================================================================
+#				      Extract Capital Letters 
+#==================================================================
+
+''' 
+Date    : Fri 17 Aug 2018 04:15:32 PM PDT 
+Subject : Lecture 6 - for loop challange
+Title   : Extract Capital Letters
+source  : https://www.udemy.com/python-the-complete-python-developer-course/learn/v4/t/quiz/4428696
+''' 
+
+# Lec 6 - for loop challange
+
+'''
+NOTES:
+
+'''
+
+quote = """
+Alright, but apart from the Sanitation the Medicine, Education, Wine,
+Public Order, Irrigation, Roads, the Fresh-Water System, 
+and Public? Health, what have the Romans ever done for us?
+"""
+
+for char in quote:
+	if char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+		print(char,end='')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
