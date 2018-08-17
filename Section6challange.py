1b7b0c33961697c5763b842b76ad289fb18943b8
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-17 12:42:22 -0700

    Add files via upload

---
+++ Section6challange.py
@@ -0,0 +1,46 @@
+#=================================================================#
+#					 Pyhton Section 6 Challange
+#-----------------------------------------------------------------#
+
+'''
+Date		: Thu 16 Aug 2018 04:43:53 PM PDT 
+Author		: Darren Gibson
+Title		: Python Section 6 Challange
+Resources	: 
+'''
+
+# Holiday invitation
+name = input('please enter your name')
+age = int(input('please enter yout age'))
+# this is written as if 17 is less than or equal to age which is less then 31
+if 17 < age < 31:
+	print('we are glad to have you')
+else:
+	print('please come back in {} years'.format(18 - age))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
