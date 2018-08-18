487030026df4c73509427cad9da2979b0e9b5ef6
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-17 17:00:05 -0700

    Add files via upload

---
+++ forloopwithstep.py
@@ -0,0 +1,108 @@
+#==================================================================
+#						for loop with a step
+#==================================================================
+
+''' 
+Date    : Fri 17 Aug 2018 04:42:43 PM PDT 
+Subject : Sec 6: Lecture 35 challange
+Title   : for loop with step
+source  : https://www.udemy.com/python-the-complete-python-developer-course/learn/v4/t/quiz/4428940
+''' 
+
+#
+
+'''
+NOTES:
+
+'''
+
+for i in range(0, 100, 7):
+	print(i)
+	
+# or with the actual math function modulus 
+
+for i in range(0, 100):
+	if i%7 == 0:
+#	if not i%7:	    this was another way that i figured out that works
+		print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
