ae8a03e7fd45a19d522b904655134e242726cd8b
Darren Gibson <25262337+DarrenGibson@users.noreply.github.com>
2018-08-17 17:44:16 -0700

    Add files via upload

---
+++ continueandbreak.py
@@ -0,0 +1,107 @@
+#==================================================================
+#						continue, break, else
+#==================================================================
+
+''' 
+Date    : Fri 17 Aug 2018 05:01:24 PM PDT 
+Subject : Program flow control
+Title   : Lec 35 - continue, break, else
+Source  : https://www.udemy.com/python-the-complete-python-developer-course/learn/v4/t/lecture/3847262?start=0
+''' 
+
+#
+
+'''
+NOTES:
+
+'''
+
+#
+shopping_List = ['milk', 'pasta', 'eggs', 'spam', 'bread', 'rice']
+for item in shopping_List:
+	if item == 'spam':
+		continue
+# remember to have the same indentation for print() as for if()
+	print('buy ' + item)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
